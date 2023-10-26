d405058fba450693840fdb02ec4e30e6cda8c2d8
ndjerrou <n.djerrou@gmail.com>
2023-10-26 09:11:29 +0100

    login controller made

--- resources/user.controller.js
+++ resources/user.controller.js
@@ -29,5 +29,19 @@ module.exports = {
       res.status(500).send({ ok: false, msg: 'Internal server error' });
     }
   },
-  login: null,
+  login: async (req, res) => {
+    // @TODO : Joi
+
+    const user = await User.findOne({ email: req.body.email }).exec();
+
+    if (!user)
+      return res.status(401).send({ ok: false, msg: 'Resource not found' });
+
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isValid)
+      return res.status(400).send({ ok: false, msg: 'Bad request' });
+
+    res.send('ok');
+  },
 };
--- routing/user.route.js
+++ routing/user.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { signup } = require('../resources/user.controller');
+const { login } = require('../resources/user.controller');
 const router = express.Router();
 
 router.route('/signup').post(signup);
+router.route('/auth').post(login);
 
 module.exports = router;
